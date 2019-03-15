6837d197c1a26beac36702a27c10f123071a9865
Mariusz Iciak <mariusz.iciak@o2.pl>
2019-03-15 18:09:53 +0100

    test

--- hello/Program.cs
+++ hello/Program.cs
@@ -11,6 +11,17 @@ namespace hello
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
+
+
+
+            foreach (var item in args)
+            {
+                Console.WriteLine("Witaj");
+                
+
+            }
             Console.Read();
         }
     }
